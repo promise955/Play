79f06c6bef671e4f263b6e0975fce01f73a351c2
Promise CHRISTOPHER <promise.christopher@totalupstreamng.coop>
2022-06-18 15:11:01 +0100

    simple

---
+++ Math.php
@@ -0,0 +1,22 @@
+<?php
+
+Class  Math {
+    function  __construct(){
+        $this->balance = 0;
+       
+    }
+
+    function topUp($amt){
+        $this->balance += $amt;
+        return  $this->balance;
+    }
+    
+    function withdraw($amt){
+        $this->balance -= $amt;
+        return  $this->balance;
+    }
+}
+
+$math  = new Math();
+echo $math->topUp(500);
+echo $math->topUp(200);
